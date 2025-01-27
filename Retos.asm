08f274525df2c37ada2f35f6d04f41fe49bbaaee
Gonto265 <felipegtupb@gmail.com>
2025-01-27 16:32:01 -0500

    Actividad 1

---
+++ Retos.asm
@@ -0,0 +1,15 @@
+/* 
+
+ACTIVIDADES: investigación
+
+Actividad 1
+
+¿Que es una computadora digital moderna?
+
+- Es un dispositiva electrónico que procesa información, utiliza códigos binarios, almacena bastante información, puede realizar cálculos aritméticos, permite escribir textos, navegación por el internet, etc.
+
+¿Cúales son sus partes?
+
+- Está compuesta por una fuente de alimentación, La placa madre, Memoria RAM, Unidad de disco duro, microprocesador, gabinete y sus respectivos conectores, cables y puertos.
+
+Actividad 2
