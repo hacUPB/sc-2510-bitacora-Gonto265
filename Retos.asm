0581920eb64a40dd0e3bfa0ebfec49035e7d6879
Gonto265 <felipegtupb@gmail.com>
2025-01-27 17:30:31 -0500

    Actividad 2 y Retos 1-5

--- Retos.asm
+++ Retos.asm
@@ -4,12 +4,75 @@ ACTIVIDADES: investigación
 
 Actividad 1
 
-¿Que es una computadora digital moderna?
+### ¿Que es una computadora digital moderna?
 
-- Es un dispositiva electrónico que procesa información, utiliza códigos binarios, almacena bastante información, puede realizar cálculos aritméticos, permite escribir textos, navegación por el internet, etc.
+- Es un dispositiva electrónico que procesa información, utiliza códigos binarios, 
+almacena bastante información, puede realizar cálculos aritméticos, permite escribir textos, 
+navegación por el internet, etc.
 
-¿Cúales son sus partes?
+### ¿Cúales son sus partes?
 
-- Está compuesta por una fuente de alimentación, La placa madre, Memoria RAM, Unidad de disco duro, microprocesador, gabinete y sus respectivos conectores, cables y puertos.
+- Está compuesta por una fuente de alimentación, La placa madre, Memoria RAM, Unidad de disco duro, 
+microprocesador, gabinete y sus respectivos conectores, cables y puertos.
 
 Actividad 2
+
+### ¿Que es entonces un programa?
+
+- El sistema operativo carga el programa en la memoria y sigue sus instrucciones, 
+las cuales son codigo escrito que conlleva a la relización de tareas especificas.
+
+### ¿Que es el lenguaje ensamblador?
+
+- El lenguaje ensamblador, compuesto por instrucciónes de basico entendimiento humano, 
+está referido a un lenguaje de codigo el cual puede entender la CPU de forma directa.
+
+### ¿Que es el lenguaje de máquina?
+
+- Se refiere al lenguaje de programación más básico, se compone de instrucciones muy específicas, 
+codificadas en formato binario, el cual puede leer y ejecutar inmediatamente la CPU.
+*/
+
+// RETOS
+
+// 1.
+
+@1978
+D=A
+
+// 2.
+
+@69
+D=A
+@100
+M=D
+
+// 3.
+
+@24
+D=M
+@200
+M=D
+
+// 4.
+
+@100
+D=M 
+@15
+D=D-A
+@100
+M=D 
+
+// 5.
+
+@0
+D=M 
+@1
+D=D+M
+@69
+D=D+A
+@2
+M=D
+
+// 6.
+
